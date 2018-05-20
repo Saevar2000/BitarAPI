491a1e8c8382f3830a1d798c0219e650d12cd9ab
Saevar2000 <saevar2000@gmail.com>
2018-05-20 07:30:58 +0000

    Add Json null value handling

--- Lightning/Models/Invoice.cs
+++ Lightning/Models/Invoice.cs
@@ -1,16 +1,25 @@
 using System;
+using Newtonsoft.Json;
 
 namespace Lightning.Models
 {
     public class Invoice
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string bolt11 { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string description { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string label { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string payment_hash { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string status { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? expires_at { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? expiry_time { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public int? msatoshi { get; set; }
     }
 }
