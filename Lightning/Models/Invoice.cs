936be294a8f3c2454a6b0b95eaffd570afd46c9f
Saevar2000 <saevar2000@gmail.com>
2018-05-20 07:21:20 +0000

    Make all integers nullable

--- Lightning/Models/Invoice.cs
+++ Lightning/Models/Invoice.cs
@@ -1,16 +1,16 @@
-using System.Collections.Generic;
+using System;
 
 namespace Lightning.Models
 {
     public class Invoice
     {
-        public string label { get; set; }
-        public string description { get; set; }
         public string bolt11 { get; set; }
+        public string description { get; set; }
+        public string label { get; set; }
         public string payment_hash { get; set; }
-        public int msatoshi { get; set; }
         public string status { get; set; }
-        public int expiry_time { get; set; }
-        public int expires_at { get; set; }
+        public int? expires_at { get; set; }
+        public int? expiry_time { get; set; }
+        public int? msatoshi { get; set; }
     }
 }
